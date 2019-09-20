2dc20415d1c65ab275f84b0f8c5e0be145c13339
hunter <hzmhunter@gmail.com>
2019-09-20 15:14:05 +0800

    目录调整

--- design/src/main/java/com/tank/design/web/design/observer/OfficialAccount.java
+++ design/src/main/java/com/tank/design/web/design/observer/OfficialAccount.java
@@ -3,19 +3,20 @@ package com.tank.design.web.design.observer;
 import java.util.Observable;
 
 /**
- * @description: 观察者模式：被观察者
  * @author hzm
- * @Date 2019/9/17
  * @version V1.0
+ * @description: 观察者模式：被观察者
+ * @Date 2019/9/17
  */
 
 public class OfficialAccount extends Observable {
 
     /**
      * 要给观察者的信息
+     *
      * @param info
      */
-    public void publishNewInfo(String info){
+    public void publishNewInfo(String info) {
         // 标识这个Observable对象已经改变，更具体来将就是把Observable中属性changed置为true
         setChanged();
 
@@ -23,6 +24,7 @@ public class OfficialAccount extends Observable {
          * 在通知所有观察者之前，需要判断Observable中属性changed是否为true，如若不为则不会发出通知。
          * 具体可以看源码，蛮好理解的。
          */
-        notifyObservers(info);;
+        notifyObservers(info);
+        ;
     }
 }
